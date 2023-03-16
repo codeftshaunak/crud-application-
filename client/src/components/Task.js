f29d0eb4997eb1ac598e2bc9ab19d9689eb0292c
Shaunak KD <shaunakkd@Shaunaks-MacBook-Air.local>
2023-03-16 12:03:36 +0600

    setup data

--- client/src/components/Task.js
+++ client/src/components/Task.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Task = () => {
+    const url = 'http://localhost:4000/api/todos';
     const [formData, setFormData] = useState({
         projectname: '',
         taskname: '',
@@ -10,7 +11,6 @@ const Task = () => {
         deadline: '',
         assigne: '',
     });
-    // const [taskList, setTaskList] = useState([]);
 
     const handleChange = (event) => {
         setFormData({
@@ -22,8 +22,9 @@ const Task = () => {
     const handleSubmit = async (event) => {
         event.preventDefault();
         try {
-            const response = await axios.post('http://localhost:4000/api/todos', formData);
+            const response = await axios.post(url, formData);
             console.log(response.data);
+
             // clear form data
             setFormData({
                 projectname: '',
@@ -38,68 +39,37 @@ const Task = () => {
         }
     };
 
-    // const handleEdit = (id, updatedTask) => {
-    //     axios.put(`http://localhost:4000/api/todos/${id}`, updatedTask)
-    //         .then((res) => {
-    //             const updatedList = taskList.map((task) => {
-    //                 if (task._id === res.data._id) {
-    //                     return res.data;
-    //                 } else {
-    //                     return task;
-    //                 }
-    //             });
-    //             setTaskList(updatedList);
-    //         })
-    //         .catch((err) => console.log(err));
-    // };
-
-    // const handleDelete = (id) => {
-    //     console.log(id);
-    //     axios.delete(`http://localhost:4000/api/todos/${id}`)
-    //         .then(() => {
-    //             const updatedList = taskList.filter((task) => task._id !== id);
-    //             setTaskList(updatedList);
-    //         })
-    //         .catch((err) => console.log(err));
-    // };
-
-    // useEffect(() => {
-    //     axios.get('http://localhost:4000/api/todos')
-    //         .then((res) => setTaskList(res.data))
-    //         .catch((err) => console.log(err));
-    // }, []);
-
-
     return (
         <>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="projectname">Project Name:</label>
-                    <input type="text" id="projectname" name="projectname" value={formData.projectname} onChange={handleChange} />
-                </div>
-                <div>
-                    <label htmlFor="taskname">Task Name:</label>
-                    <input type="text" id="taskname" name="taskname" value={formData.taskname} onChange={handleChange} />
-                </div>
-                <div>
-                    <label htmlFor="taskdescription">Task Description:</label>
-                    <textarea id="taskdescription" name="taskdescription" value={formData.taskdescription} onChange={handleChange}></textarea>
-                </div>
-                <div>
-                    <label htmlFor="acceptancecriteria">Acceptance Criteria:</label>
-                    <textarea id="acceptancecriteria" name="acceptancecriteria" value={formData.acceptancecriteria} onChange={handleChange}></textarea>
-                </div>
-                <div>
-                    <label htmlFor="deadline">Deadline:</label>
-                    <input type="date" id="deadline" name="deadline" value={formData.deadline} onChange={handleChange} />
-                </div>
-                <div>
-                    <label htmlFor="assigne">Assignee:</label>
-                    <input type="text" id="assigne" name="assigne" value={formData.assigne} onChange={handleChange} />
-                </div>
-                <button type="submit">Create Task</button>
-            </form>
-
+            <div className="crud"><h1>Assign Task</h1>
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        <label htmlFor="projectname">Project Name:</label>
+                        <input type="text" id="projectname" name="projectname" value={formData.projectname} onChange={handleChange} required />
+                    </div>
+                    <div>
+                        <label htmlFor="taskname">Task Name:</label>
+                        <input type="text" id="taskname" name="taskname" value={formData.taskname} onChange={handleChange} required />
+                    </div>
+                    <div>
+                        <label htmlFor="taskdescription">Task Description:</label>
+                        <textarea id="taskdescription" name="taskdescription" value={formData.taskdescription} onChange={handleChange} required></textarea>
+                    </div>
+                    <div>
+                        <label htmlFor="acceptancecriteria">Acceptance Criteria:</label>
+                        <textarea id="acceptancecriteria" name="acceptancecriteria" value={formData.acceptancecriteria} onChange={handleChange} required></textarea>
+                    </div>
+                    <div>
+                        <label htmlFor="deadline">Deadline:</label>
+                        <input type="date" id="deadline" name="deadline" value={formData.deadline} onChange={handleChange} required />
+                    </div>
+                    <div>
+                        <label htmlFor="assigne">Assignee:</label>
+                        <input type="text" id="assigne" name="assigne" value={formData.assigne} onChange={handleChange} required />
+                    </div>
+                    <button type="submit">Create Task</button>
+                </form>
+            </div>
             {/* <div className="task__list">
                 {taskList.map((task) => (
                     <div key={task._id}>
